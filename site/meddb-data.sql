e612cb06a2aefc6d3f656814a4df0aee6e9b54d7
Vipin <vipina82@gmail.com>
2015-04-01 21:20:48 +0530

    MockUI and database ddl,data added

---
+++ site/meddb-data.sql
@@ -0,0 +1,16 @@
+﻿insert into Patient values('ankitk@iitrpr.ac.in', 'Ankit Khokhar','male','1993-01-26' );
+insert into Patient values('anshuman@gmail.com', 'Anshuman Yadav','male','1994-04-23' );
+insert into Patient values('balwinder@iitrpr.ac.in', 'Balwinder Sodhi','male','1973-01-26' );
+insert into Patient values('ranjana@iitrpr.ac.in', 'Ranjana Sodhi','female','1983-01-26' );
+
+
+insert into Student values('ankitk@iitrpr.ac.in', '2012CSB1089','Mercury-A','317','abcd','9234567890',null,null,null,null );
+insert into Student values('anshuman@gmail.com', '2012CSB1047','Mercury-A','217','9345678901','123','Alwar','Rajasthan',12342 );
+
+
+insert into Employee values('balwinder@iitrpr.ac.in',null,'Chandigarh','Punjab',12345);
+insert into Employee values('ranjana@iitrpr.ac.in','123-A','Mohali','Punjab',1235);
+
+
+insert into Faculty values ('balwinder@iitrpr.ac.in','Computer Science');
+insert into Faculty values ('ranjana@iitrpr.ac.in','Electrical');
